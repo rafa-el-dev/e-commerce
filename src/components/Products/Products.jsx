35f69ff5615807152809c74d04740ec9fe38246e
Rafael Ribeiro <109827006+rafa-el-dev@users.noreply.github.com>
2023-09-24 20:03:54 +0100

    (feat): add components Product and Products

---
+++ src/components/Products/Products.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { Grid } from "@material-ui/core";
+import Product from "./Product/Product";
+
+const products = [
+  {
+    id: 1,
+    name: "Shoes",
+    description: "Running shoes.",
+    price: "$5",
+    image:
+      "https://media.gq-magazine.co.uk/photos/64d4c75cec043c4e3762c817/master/w_1600,c_limit/Jordan%202.jpg",
+  },
+  {
+    id: 2,
+    name: "Macbook",
+    description: "Apple Macbook.",
+    price: "$25",
+    image:
+      "https://cdn.mos.cms.futurecdn.net/Nos3e7TU4vQJUeGF7jkrBd-970-80.jpg.webp",
+  },
+];
+
+const Products = () => {
+  return (
+    <main>
+      <Grid container justify="center" spacing={4}>
+        {products.map((product) => (
+          <Grid key={product.id} item xs={12} sm={8} md={4} lg={3}>
+            <Product product={product} />
+          </Grid>
+        ))}
+      </Grid>
+    </main>
+  );
+};
+
+export default Products;
