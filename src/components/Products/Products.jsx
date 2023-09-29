634feec6c21e3410529c7dbe1978db6049aeb6e1
Rafael Ribeiro <109827006+rafa-el-dev@users.noreply.github.com>
2023-09-29 16:20:10 +0100

    (refactor): add new products and finalize the cards in products componen

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -3,26 +3,7 @@ import { Grid } from "@material-ui/core";
 import Product from "./Product/Product";
 import useStyles from "./styles";
 
-const products = [
-  {
-    id: 1,
-    name: "Shoes",
-    description: "Running shoes.",
-    price: "$5",
-    image:
-      "https://media.gq-magazine.co.uk/photos/64d4c75cec043c4e3762c817/master/w_1600,c_limit/Jordan%202.jpg",
-  },
-  {
-    id: 2,
-    name: "Macbook",
-    description: "Apple Macbook.",
-    price: "$25",
-    image:
-      "https://cdn.mos.cms.futurecdn.net/Nos3e7TU4vQJUeGF7jkrBd-970-80.jpg.webp",
-  },
-];
-
-const Products = () => {
+const Products = ({ products }) => {
   const classes = useStyles();
   
   return (
---
+++ src/lib/commerce.js
@@ -0,0 +1,4 @@
+import Commerce from "@chec/commerce.js";
+
+// export const commerce = new Commerce(process.env.REACT_APP_CHEC_PUBLIC_KEY, true);
+export const commerce = new Commerce('pk_test_54246cee21334a6c53271cbf42d27dc13fff964966dfe', true);
